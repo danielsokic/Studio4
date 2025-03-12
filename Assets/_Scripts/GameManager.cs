9fef50e4f19814f27a1038f67a7517ef93270c7b
Daniel Sokic <44309771+danielsokic@users.noreply.github.com>
2025-03-11 17:49:33 -0700

    SettingsMenu

--- Assets/_Scripts/GameManager.cs
+++ Assets/_Scripts/GameManager.cs
@@ -6,13 +6,45 @@ public class GameManager : SingletonMonoBehavior<GameManager>
     [SerializeField] private CoinCounterUI coinCounter;
     [SerializeField] private int score = 0;
     [SerializeField] private TextMeshProUGUI scoreText;
+    [SerializeField] private InputManager inputManager;
+    [SerializeField] private GameObject settingsMenu;
+    private bool isSettingsMenuActive;
+    public bool IsSettingsMenuActive => isSettingsMenuActive;
 
     protected override void Awake()
     {
         base.Awake();
         Cursor.visible = false;
         Cursor.lockState = CursorLockMode.Locked;
+        inputManager.OnSettingsMenu.AddListener(ToggleSettingsMenu);
+        DisableSettingsMenu();
     }
+
+    private void ToggleSettingsMenu(){
+        Time.timeScale = 0f;
+        settingsMenu.SetActive(true);
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+        isSettingsMenuActive = true;
+    }
+
+    public void DisableSettingsMenu(){
+        Time.timeScale = 1f;
+        settingsMenu.SetActive(false);
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+        isSettingsMenuActive = false;
+    }
+
+    public void quitGame()
+{
+    #if UNITY_EDITOR
+    UnityEditor.EditorApplication.isPlaying = false;
+    #else
+    Application.Quit();
+    #endif
+}
+
 
     public void IncreaseScore()
     {
